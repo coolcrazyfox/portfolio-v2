2c408bd6da942d45282e8b6ee438294d95ed74bb
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-11 23:32:07 +0300

    add navBar icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon4.svg';
-import navIcon3 from '../assets/img/nav-icon5.svg';
+import navIcon3 from '../assets/img/nav-icon6.svg';
 
 export const NavBar = () => {
 
@@ -45,7 +45,7 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="" /></a>
+              <a href="https://www.linkedin.com/in/andrei-malets-072827210/"><img src={navIcon1} alt="" /></a>
               <a href="#"><img src={navIcon2} alt="" /></a>
               <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
