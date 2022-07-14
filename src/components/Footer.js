173649d59dc2f853bd720aff20ba312ff187f9cf
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-14 22:14:34 +0300

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ export const Footer = () => {
               <a href="https://t.me/Andrei_Malets/"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
