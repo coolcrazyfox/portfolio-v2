c6c38868e0258f2d7872471833ab9e803cdba4c0
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-14 22:12:49 +0300

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/andrei-malets-072827210/"><img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
