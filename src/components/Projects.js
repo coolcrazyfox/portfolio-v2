12e6f21678e00687b6515e16533a729a53c65ea6
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-16 22:59:34 +0300

    icons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg1 from "../assets/img/social.gif";
+import projImg2 from "../assets/img/todolist.gif";
+import projImg3 from "../assets/img/webSite.gif";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -178,7 +178,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} ></img>
     </section>
   )
 }
