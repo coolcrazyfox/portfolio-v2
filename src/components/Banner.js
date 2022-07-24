d5a71c0820c8d84377e36d80f994b906ca7d6fc5
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-24 22:42:44 +0300

    change header-img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
+// import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/1.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -77,7 +78,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
+                  <img className="my-photo" src={headerImg} alt="Header Img"/>
                 </div>}
             </TrackVisibility>
           </Col>
